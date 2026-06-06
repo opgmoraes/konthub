76bd01bf17751741a4d8bc2f52ae9407c4a8b105
Pedro Gabriel Gomes <pedrogm.dev@gmail.com>
2026-06-06 16:46:58 -0300

    add auth google

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // COLE AQUI O SEU FIREBASE CONFIG (esse abaixo é só um exemplo)
@@ -18,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 // Exporta as funções para usarmos nas telas do sistema
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
